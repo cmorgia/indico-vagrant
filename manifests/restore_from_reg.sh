48d4c449925ab1f48ea990750370cb4049f2c35f
Claudio Morgia <cmorgia@gmail.com>
2016-05-29 22:40:24 +0200

    Remote restore from reg

---
+++ manifests/restore_from_reg.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+cd /vagrant/opt
+source bin/activate
+
+BACKUP_FILE = $(ssh -q reg "cd /opt/new/indico-instance/backup ; ls -1tr | tail -1")
+BACKUP_NAME = $(basename $BACKUP_FILE .zip)
+
+if [ ! -d "/opt/indico/backup" ]; then
+	mkdir -p /opt/indico/backup
+fi
+
+scp reg:/opt/new/indico-instance/backup/$BACKUP_FILE /opt/indico/backup
+
+/vagrant/opt/indico-src/bin/unog/restore.sh -p $BACKUP_FILE -force
+/vagrant/opt/indico-src/bin/unog/restore.sh -r $BACKUP_NAME -force
